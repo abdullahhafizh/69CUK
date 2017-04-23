476b14e6ca6251fbf78e1d03c8d8a3cd141a3b60
Hi-Fiez <hifiez.soft@gmail.com>
2017-04-23 19:33:39 +0700

    col-md home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,39 @@
 
 @section('header')
 <title>69CUK For Adult Only</title>
+<style type="text/css">
+    .verticalline {    
+      border-right: groove;
+  }
+</style>
 @endsection
-@section('content')      
-<center>  
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-    <h1>qwretyuio</h1>
-</center>  
-
+@section('content')  
+<div class="main">
+    <div class="container">
+        <div class="row">    
+        <div class="col-sm-9 col-md-9 verticalline">                    
+            </div>                    
+            <div class="col-sm-3 col-md-3">
+             <center>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+                 <h1>IKLAN</h1>
+             </center>
+         </div>
+     </div>
+ </div>
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 
 @endsection
